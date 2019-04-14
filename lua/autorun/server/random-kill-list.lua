09d3e21ecf0fd1ea1a72bd6b512f63e5579815aa
semklauke <privat@semklauke.de>
2019-04-14 04:49:57 +0200

    document structure

--- lua/autorun/server/random-kill-list.lua
+++ lua/autorun/server/random-kill-list.lua
@@ -2,4 +2,39 @@ local sqlite3 = require("lsqlite3complete")
 
 local config = require("config")
 
-local db = sqlite3.open(config.SQLITE_PATH)
+local database = assest( sqlite3.open(config.SQLITE_PATH) )
+
+
+-- globals --
+
+local players = {} -- All players in the round
+local traitors = {} -- All traitors in the round
+local votes = {}
+local voteCounts = {}
+local traitorKillers = {}
+local messages = {}
+
+
+-- helper functionn --
+
+local function round(n)
+    return n % 1 >= 0.5 and math.ceil(n) or math.floor(n)
+end
+
+local function checkDB()
+    return database:isopen()
+end
+
+
+-- network communication to the client --
+
+-- database maniplulation --
+
+-- game hooks -- 
+
+-- sqlite setup --
+
+local sqlitesetup -- = require("setup-sqlite")
+--sqlitesetup.setup(database)
+
+-- timer setup --
