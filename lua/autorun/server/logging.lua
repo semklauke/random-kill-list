cbc9e00bd5200e4ca03fe11eeff21f6afe4e8b04
semklauke <privat@semklauke.de>
2019-04-14 17:20:51 +0200

    logging module export

--- lua/autorun/server/logging.lua
+++ lua/autorun/server/logging.lua
@@ -22,3 +22,5 @@ function module.error.statement_execute(statement_name, db_errormsg)
     module.out(module.prefix .. "Statement " .. statement_name .. " not excuted with error")
     module.out(db_errormsg)
 end
+
+return module
