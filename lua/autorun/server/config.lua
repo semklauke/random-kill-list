8b88055fe65ae21f6bc72d0e3b06e4aa2092f0cf
semklauke <privat@semklauke.de>
2019-04-14 03:13:41 +0200

    vote perc. added

--- lua/autorun/server/config.lua
+++ lua/autorun/server/config.lua
@@ -7,9 +7,13 @@ local config = {}
 
 config.SQLITE_FILE="randomKillsList.db"
 config.WEBVIEW_FOLDER="randome-kill-list-webview/" -- don't forget trailing /
+
+config.VOTE_PERC = 0.8
+config.SEND_KEY = "SHJKDGKASJDHSKLADG&@%&DSKLJAHLBDKBA<SBD"
+
 -- Only edit of you know what you are doing
 config.RESOURCE_PATH="../../../resource/"
 config.SQLITE_PATH=config.RESOURCE_PATH .. config.SQLITE_FILE
 config.WEBVIEW_PATH=config.RESOURCE_PATH .. config.WEBVIEW_FOLDER
 
-return M
+return config
