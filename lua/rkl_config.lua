83a12f08edc3106c6bb07e2296df0c156a539dd6
semklauke <privat@semklauke.de>
2019-04-16 22:53:55 +0200

    fixed table name

--- lua/rkl_config.lua
+++ lua/rkl_config.lua
@@ -11,7 +11,7 @@ config.SEND_KEY = "SHJKDGKASJDHSKLADG&@%&DSKLJAHLBDKBA<SBD"
 
 config.table = {
     player = "rkl_player",
-    random_kills = "rkl_player",
+    random_kills = "rkl_random_kills",
     rounds_played = "rkl_rounds_played"
 }
 
